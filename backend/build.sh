6d3c2bd4223ee3ecf652df1664d21bc87fe43ffc
ming xiong <iona.m.xiong@gmail.com>
2022-12-21 07:44:17 +0200

    change the package install sequence (numpy seems should be installed earlier than pandas )

--- backend/build.sh
+++ backend/build.sh
@@ -2,13 +2,20 @@ rm function.zip
 
 python3.9 -m pip install --upgrade pip
 
+pip install virtualenv
+
+virtualenv venv --python=python3.9
+
 python3.9 -m pip install \
     --platform manylinux2014_x86_64 \
     --target=./venv/lib/python3.9/site-packages \
     --implementation cp \
     --python 3.9 \
     --only-binary=:all: --upgrade \
-    pandas numpy==1.23.5 fastapi mangum
+    -r requirements.txt 
+
+# if the numpy failed the installation, try to run the commented command below 
+# pip install -r requirements.txt --force-reinstall
 
 # cd venv/Lib/site-packages/
 cd ./venv/lib/python3.9/site-packages/
